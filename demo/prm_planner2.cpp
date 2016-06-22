d596cc46de7afc51da7040fc487702ebf0fe5140
Lakshay Garg <lakshayg@iitk.ac.in>
2016-06-22 14:15:44 +0530

    Added A* search

---
+++ demo/prm_planner2.cpp
@@ -0,0 +1,39 @@
+#include <mpel/core.hpp>
+#include <mpel/builtins.hpp>
+#include <iostream>
+
+using namespace mpel;
+
+int main(int argc, char **argv) {
+
+	if (argc < 2) {
+		std::cerr << "Demo requires a map image" << std::endl;
+		return -1;
+	}
+
+	Planner::Config pc;
+	pc.graph_search = a_star_search();
+	pc.graph_builder = probabilistic_graph_builder(100);
+	pc.interpolator = default_interpolator();
+
+	Planner p(pc);
+	Workspace ws;
+	ws.map = load_map_from_image(argv[1]);
+	p.load_workspace(ws);
+
+	ProblemDefinition pdef;
+	pdef.start = mark_point(ws);
+	pdef.goal = Point(400,400);
+
+	Path path = p.solve(pdef);
+
+	View v;
+	v.add_layer(p);
+	v.add_layer(pdef);
+	v.add_layer(path);
+	View::stay();
+
+	v.save();
+
+	return 0;
+}
--- lib/builtins/graph_searches/a_star_search.cpp
+++ lib/builtins/graph_searches/a_star_search.cpp
@@ -1,16 +1,91 @@
 #include <mpel/builtins.hpp>
+#include <functional>
+#include <algorithm>
+#include <iostream>
 #include <vector>
+#include <array>
 #include <queue>
+#include <cmath>
 #include "types.hpp"
 #include "common.hpp"
 
+#define LOG(msg) {std::cout << msg << std::endl;}
+
 namespace mpel {
 // a star search
-a_star_search::a_star_search() {
-	throw UnimplementedException();
+a_star_search::a_star_search() {}
+
+double dist(GraphRef g, size_t i, size_t j) {
+	Point a = g.vertex(i);
+	Point b = g.vertex(j);
+	double dx = a.x - b.x;
+	double dy = a.y - b.y;
+	return sqrt(dx*dx + dy*dy);
 }
+
+bool contains(const std::vector<size_t>& v, size_t e) {
+	for (size_t x : v) if (x == e) return true;
+	return false;
+}
+
 Path a_star_search::operator()(GraphRef g, PointRef a, PointRef b) {
 	Path path;
+
+	size_t in = g.descriptor(a);
+	size_t out = g.descriptor(b);
+
+	std::vector<size_t> open, closed;
+	open.reserve(g.num_vertices());
+	closed.reserve(g.num_vertices());
+
+	std::vector<double> fcost(g.num_vertices(), std::numeric_limits<double>::max());
+	std::vector<double> gcost(g.num_vertices(), std::numeric_limits<double>::max());
+	std::vector<size_t> parent(g.num_vertices(), g.num_vertices());
+	fcost[in] = dist(g, in, out);
+	gcost[in] = 0;
+	parent[in] = in;
+	open.push_back(in);
+
+	while (not open.empty()) {
+		// pick the element in open list with minimum fcost
+		size_t curr = open.front();
+		double min_cost = fcost[curr];
+		for (auto e : open) {
+			if (fcost[e] <= min_cost) {
+				min_cost = fcost[e];
+				curr = e;
+			}
+		}
+
+		// remove curr from open list and insert in the closed list
+		open.erase(std::remove(open.begin(), open.end(), curr), open.end());
+		closed.push_back(curr);
+
+		if (curr == out) {
+			while (parent[curr] != curr) {
+				path.push_back(g.vertex(curr));
+				curr = parent[curr];
+			}
+			path.push_back(g.vertex(curr));
+			std::reverse(path.begin(), path.end());
+			return path;
+		}
+
+		// assign cost to all neighbors of curr
+		for (size_t n = 0; n < g.num_vertices(); ++n) {
+			if (g.weight(n, curr) <= 0) continue;
+			if (contains(closed, n)) continue;
+
+			double tmp_g = gcost[curr] + g.weight(curr, n);
+			if (tmp_g < gcost[n]) {
+				gcost[n] = tmp_g;
+				fcost[n] = gcost[n] + dist(g, out, n);
+				parent[n] = curr;
+			}
+			if (not contains(open, n)) open.push_back(n);
+		}
+	}
+
 	return path;
 }
 }
