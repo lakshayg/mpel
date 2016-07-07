e01356153821c81413c2e274a51382e017a1f650
Lakshay Garg <lakshayg@iitk.ac.in>
2016-07-08 02:10:29 +0530

    Fixed BFS [Issue 12]

--- lib/builtins/graph_searches/breadth_first.cpp
+++ lib/builtins/graph_searches/breadth_first.cpp
@@ -16,36 +16,33 @@ namespace builtin {
             size_t in = g.descriptor(a);
             size_t out = g.descriptor(b);
 
-            double max_dist = std::numeric_limits<double>::max();
-            std::vector<double> dist(g.num_vertices(), max_dist);
             std::vector<size_t> parent(g.num_vertices());
+            std::vector<bool> visited(g.num_vertices());
             std::queue<size_t> Q;
             Q.push(in);
-
-            dist[in] = 0;
-            parent[in] = in;
+            visited[in] = true;
+            parent[in] = g.num_vertices();
 
             while (not Q.empty()) {
                 size_t curr = Q.front();
                 Q.pop();
-
-                for (size_t i = 0; i < g.num_vertices(); ++i) {
-                    if (g.weight(curr, i) < 0) continue; // not connected
-                    if (dist[i] == max_dist) {
-                        dist[i] = dist[curr] + g.weight(curr, i);
-                        parent[i] = curr;
-                        Q.push(i);
-                        // if this is the goal
-                        if (curr == out) {
-                            while (parent[curr] != curr) {
-                                path.push_back(g.vertex(curr));
-                                curr = parent[curr];
-                            }
-                            path.push_back(g.vertex(curr));
-                            std::reverse(path.begin(), path.end());
-                            return path;
-                        }
+                if (curr == out) {
+                    while (parent[curr] != g.num_vertices()) {
+                        path.push_back(g.vertex(curr));
+                        curr = parent[curr];
                     }
+                    path.push_back(g.vertex(curr));
+                    std::reverse(path.begin(), path.end());
+                    return path;
+                }
+
+                // push all the neighbors of curr into the queue
+                for (size_t i = 0; i < g.num_vertices(); ++i) {
+                    // if not connected or already visited
+                    if ((g.weight(curr, i) < 0) or visited[i]) continue;
+                    visited[i] = true;
+                    parent[i] = curr;
+                    Q.push(i);
                 }
             }
             return path;
