f39bf77f216ee6c25989fd3daca76d66d5e337ee
Lakshay Garg <lakshayg@iitk.ac.in>
2016-08-11 20:32:10 +0530

    Fixed bug in planner (Closes #18)

--- lib/planner.cpp
+++ lib/planner.cpp
@@ -79,25 +79,34 @@ Path Planner::solve(ProblemDefinition pdef)
         }
     }
 
-    Path ret;
-    if (in_segment == 0 or out_segment == 0) { // If the terminal points could not be successfully inserted
+    auto t0 = high_resolution_clock::now();
+
+    // check if the terminal points could not be successfully inserted
+    // it may happen that they could not be inserted because the graph
+    // built by graph builder is not sufficiently dense or the user did
+    // not use a graph builder at all due to which the graph is empty
+    if (in_segment == 0 or out_segment == 0) {
         std::cout << "[Planner::solve] Terminal points could not be inserted in the graph, "
                   << "check if the graph is sufficiently dense" << std::endl;
+        // we return a straight line joining start and goal hoping that the interpolator
+        // will be able to find a path. If it too cannot find a path then the returned
+        // path is a straight line between start and goal points.
+        p.push_back(pdef.start);
+        p.push_back(pdef.goal);
     } else {
-
-        auto t0 = high_resolution_clock::now();
         p = _pc.graph_search(tmp_g, pdef.start, pdef.goal);
-        auto t1 = high_resolution_clock::now();
-        t_search = duration_cast<microseconds>(t1 - t0).count();
-
-        t0 = high_resolution_clock::now();
-        ret = (p.size() > 0 ? _pc.interpolator(_ws.map, p) : p);
-        t1 = high_resolution_clock::now();
-        t_interp = duration_cast<microseconds>(t1 - t0).count();
     }
 
-    if (ret.size() == 0) {
-        std::cout << "[Planner::solve] No path found! Returning empty path" << std::endl;
+    auto t1 = high_resolution_clock::now();
+    t_search = duration_cast<microseconds>(t1 - t0).count();
+
+    t0 = high_resolution_clock::now();
+    Path ret = _pc.interpolator(_ws.map, p);
+    t1 = high_resolution_clock::now();
+    t_interp = duration_cast<microseconds>(t1 - t0).count();
+
+    if (ret.back() != pdef.goal) { // check if path was completely interpolated
+        std::cout << "[Planner::solve] Could not find a path!" << std::endl;
     }
 
     return ret;
